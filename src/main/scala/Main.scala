c1cb800c849d4ba2c1e0fcb267cfaf800ef0dd38
alexadre <pamahe@proton.me>
2023-11-16 09:30:58 +0100

    Modify the main function. Add if-else structure to handle args size : 0 arg -> print "REPL" 1 arg -> path to code source file, read it and print lines. More args -> print error

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,31 @@
 package fr.eseo.lang
 
+import scala.io.Source
+
 object Main {
   def main(args: Array[String]): Unit = {
     val test = "var x = 4 ;"
-    println(f(test))
+
+    if (args.size == 0) {
+      println("REPL")
+    } else if (args.size == 1) {
+      runFile(args(0))
+    } else {
+      println("Usage: jeseo [script]")
+      System.exit(64)
+    }
   }
 
-  def f(input: String): List[String] = {
+
+
+  private def runFile(path: String): Unit = {
+
+    val bufferedIterator = Source.fromFile(path)
+    bufferedIterator.getLines().foreach(line => println(f(line)))
+    bufferedIterator.close()
+  }
+
+  private def f(input: String): List[String] = {
     input.split("\\s").toList
   }
 
